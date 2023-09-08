b26a8d59ae2d5474bdd86535d4e4ef22a6d19d9e
jatinbhalla <55462128+jatinbhalla@users.noreply.github.com>
2023-09-08 15:30:11 +0530

    Update 1.ps1

--- 1.ps1
+++ 1.ps1
@@ -1,7 +1,4 @@
-$output='{"value1": "abc",
-"value2": "def"
-
-}'
+$output='{"value1": "abc","value2": "def"}'
 $outputsummary = $output | ConvertTo-json
 write-output "here it is : $outputsummary"
 "outputsummary = $($outputsummary)">> $env:GITHUB_ENV
