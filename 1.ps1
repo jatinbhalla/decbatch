9c059c293f3cacdfb9e3a9b8817db6504f8bd045
jatinbhalla <55462128+jatinbhalla@users.noreply.github.com>
2023-09-08 14:19:50 +0530

    Create 1.ps1

---
+++ 1.ps1
@@ -0,0 +1,7 @@
+output='{"value1": "abc",
+"value2": "def"
+
+}
+$outputsummary = $output | ConvertTo-json
+write-output "here it is : $outputsummary"
+"parameters.outputsummary.value = $($outputsummary)">> $env:GITHUB_ENV
