40d62553b613c2302f5deff22f4e1f11b24b926c
jatinbhalla <55462128+jatinbhalla@users.noreply.github.com>
2023-09-11 13:45:28 +0530

    Update 1.ps1

--- 1.ps1
+++ 1.ps1
@@ -1,1 +1,15 @@
-"outputsummaryenv = a ">> $GITHUB_ENV
+[CmdLetBinding()]
+param(
+    # Mandatory params
+    [PSObject] $outputsummary 
+    
+)
+try {
+    Write-Host "this is summary $outputsummary"
+    }
+
+# Global catch for any error
+catch { 
+    $_
+    Write-Information "error occurred, details above."
+}
